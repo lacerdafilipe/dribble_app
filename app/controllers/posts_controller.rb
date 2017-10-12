4b30472149ee8e8a7f1c313d5941024295089cc4
Filipe <lacerdafilipe@gmail.com>
2017-10-13 00:15:38 +0100

    Created the method new, show, create, edit, update and destroy and partial form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,42 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
-    @post = Post.all.order
+    @post = Post.all
   end
 
   def new
     @post = Post.new
   end
 
+  def show
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
   private
-    def permit
+    def find_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :link, :description)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :post
+  resources :posts
 
   root  "posts#index"
 end
